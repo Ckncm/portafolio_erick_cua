dd8a3d45295bacbe20c16f19951004a53d7c993b
Ckncm <Ckncm@users.noreply.github.com>
2026-08-10 03:14:49 +0000

    deploy: 834d45c1f92384e7d35b717888c60a55c5894da1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2829556595" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1649616712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
